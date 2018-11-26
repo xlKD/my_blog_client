b38c743fdb7a89794d68228e67351e46d7183b24
NGUYEN QUOC HUNG <hungxlkd@gmail.com>
2018-11-26 17:08:22 +0900

    Due to low perf of gcp compute engine, add build to git

---
+++ build/precache-manifest.7f9ec9fd7408f79340c235df099f74d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56df57fcb955cbccc762",
+    "url": "/static/js/main.56df57fc.chunk.js"
+  },
+  {
+    "revision": "aafca6b52f45581c6662",
+    "url": "/static/js/1.aafca6b5.chunk.js"
+  },
+  {
+    "revision": "56df57fcb955cbccc762",
+    "url": "/static/css/main.2c1e24ce.chunk.css"
+  },
+  {
+    "revision": "97f5db6d65eb2422f8511e79b6706077",
+    "url": "/index.html"
+  }
+];
