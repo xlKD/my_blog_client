494135d51947a40e19043ac47c483a307b367634
NGUYEN QUOC HUNG <hungxlkd@gmail.com>
2018-11-26 19:29:06 +0900

    axios http -> https

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -67,7 +67,7 @@ class Posts extends Component {
   }
 
   async componentDidMount() {
-    const posts = (await axios.get('http://127.0.0.1:3000/posts/api')).data;
+    const posts = (await axios.get('https://127.0.0.1:3000/posts/api')).data;
     this.setState({
       posts,
     });
